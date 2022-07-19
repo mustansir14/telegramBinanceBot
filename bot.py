3655482fe61386c2c1431b30856e4b1e72d11037
Mustansir Muzaffar <mustansir2001@gmail.com>
2022-07-19 14:15:23 +0500

    done

---
+++ bot.py
@@ -0,0 +1,28 @@
+from binance.client import Client
+import time
+from config import *
+import requests
+
+def send_message(message, bot_token, chat_ids):
+
+    responses = []
+    for chat_id in chat_ids: 
+        responses.append(requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", data={"chat_id": chat_id, "text":message}))
+    return responses
+
+client = Client()
+
+exchange_info = client.get_exchange_info()
+previous_trading_pairs = [s["symbol"] for s in exchange_info['symbols']]
+
+while True:
+    time.sleep(TIME_INTERVAL)
+    exchange_info = client.get_exchange_info()
+    trading_pairs = [s["symbol"] for s in exchange_info['symbols']]
+    new_pairs = list(set(trading_pairs) - set(previous_trading_pairs))
+    if new_pairs:
+        for pair in new_pairs:
+            send_message(pair + " - new pair listing", TELEGRAM_BOT_TOKEN, CHAT_IDS)
+    else:
+        print("No new pairs")
+    previous_trading_pairs = trading_pairs
---
+++ config.example.py
@@ -0,0 +1,3 @@
+TELEGRAM_BOT_TOKEN = "YOUR TOKEN"
+CHAT_IDS = [] # The chat IDs to which messages need to be sent
+TIME_INTERVAL = 600 # in seconds
